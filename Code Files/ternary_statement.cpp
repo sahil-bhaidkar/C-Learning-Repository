f5badef59ddd742660a17008e05c95062590d193
Sahil Bapu Bhaidkar <44014705+sahil-bhaidkar@users.noreply.github.com>
2025-01-04 20:52:24 +0530

    Create ternary_statement.cpp

---
+++ Code Files/ternary_statement.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// This is the starting point of the code.
+int main(){
+    int n;
+    cout << "Enter your numer you want to check : ";
+    cin >>n;
+    
+    cout << (n>=0 ? "Entered Number is positive.":"Entered Number is negate.") <<endl;
+    return 0;
+    
+}
+
+// Boilerplate code -comment
