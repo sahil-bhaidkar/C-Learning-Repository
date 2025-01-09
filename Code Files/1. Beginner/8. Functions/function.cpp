fef7fe54c4c4aafe97de41f0a53167f89ac68574
Sahil Bapu Bhaidkar <44014705+sahil-bhaidkar@users.noreply.github.com>
2025-01-09 21:26:26 +0530

    Create function.cpp

---
+++ Code Files/1. Beginner/8. Functions/function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//function definition :
+int printhello(){
+    cout << "Hello World!\n";
+    return 10;
+}
+
+int main()
+{
+    //int n = printhello();
+    //cout << "val : " << n << endl; or
+    cout << "Val : " << printhello() << endl;
+    return 0;
+}
